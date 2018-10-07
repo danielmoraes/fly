cb515efbbf9573c62a8d42b2177ae5dc741bb3f1
Daniel Moraes <daniel.b.moraes@gmail.com>
2018-10-06 22:30:00 -0300

    Custom query params for azul search.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import express from 'express'
 import moment from 'moment'
 
+const yup = require('yup')
+
 import { Azul } from './api'
 
 const app = express()
 const port = 3000
 
 app.get('/azul', async (req, res) => {
-  const response = await Azul.search('sao', 'aju', moment('2018-12-15'))
-  res.send(response)
+  const schema = yup.object().shape({
+    origin: yup.string().required(),
+    destination: yup.string().required(),
+    date: yup.date().default(() => moment().add(15, 'd'))
+  })
+
+  try {
+    const data  = await schema.cast(req.query)
+    const response = await Azul.search(data.origin, data.destination, data.date)
+    res.send(response)
+  } catch (e) {
+    res.sendStatus(400)
+  }
 })
 
 
