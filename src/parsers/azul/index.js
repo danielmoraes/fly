2132d162d50eb8c70a8a0eaafa8b34277e13ae99
Daniel Moraes <daniel.b.moraes@gmail.com>
2018-10-19 23:01:26 -0300

    Update Azul parser.

--- src/parsers/azul/index.js
+++ src/parsers/azul/index.js
@@ -4,7 +4,7 @@ import { map, min } from 'lodash'
 const AzulParser = {
   findBestPrice: data => {
     const $ = cheerio.load(data)
-    const prices = map($('.area-radio .price .fare-price'), element => {
+    const prices = map($('span.fare-price'), element => {
       let price = element.children[0].data
       price = price.replace('.', '')
       price = price.replace(',', '.')
